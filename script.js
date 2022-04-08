451549caabc21cbe8182d10cff795b938a05c4c5
CaioVitor1 <caiovitor33@hotmail.com>
2022-04-08 15:11:31 -0300

    first commit: desenvolvendo layout

---
+++ script.js
@@ -0,0 +1,5 @@
+//let quantidade = prompt("Com quantas cartas você quer jogar?")
+//while((quantidade < 4) || (quantidade > 14)  || ( (quantidade%2) !== 0) ) {
+   // quantidade = prompt("Quantidade inválida. Escolha um número par entre 4 e 14 cartas")
+//}
+let cartas = []
