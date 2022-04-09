e25b6957b2ef07b8d0b216378054dcf81fa0f4d2
CaioVitor1 <caiovitor33@hotmail.com>
2022-04-09 20:24:22 -0300

    adicionando funções

--- script.js
+++ script.js
@@ -1,7 +1,63 @@
-let quantidade = prompt("Com quantas cartas você quer jogar?")
-while((quantidade < 4) || (quantidade > 14)  || ( (quantidade%2) !== 0) ) {
-    quantidade = prompt("Quantidade inválida. Escolha um número par entre 4 e 14 cartas")
+// Variáveis globais
+let quantidade;
+const imagensCartas = ["imagens/bobrossparrot.gif","imagens/bobrossparrot.gif",
+                        "imagens/explodyparrot.gif","imagens/explodyparrot.gif",
+                        "imagens/fiestaparrot.gif","imagens/fiestaparrot.gif",
+                        "imagens/metalparrot.gif","imagens/metalparrot.gif",
+                        "imagens/revertitparrot.gif","imagens/revertitparrot.gif",
+                        "imagens/tripletsparrot.gif","imagens/tripletsparrot.gif",
+                        "imagens/unicornparrot.gif","imagens/unicornparrot.gif"]
+
+function jogar() {
+
+function selecionandoQuantidade() {
+    quantidade = parseInt(prompt("Com quantas cartas você quer jogar?"))      
 }
+selecionandoQuantidade()
+
+while((quantidade < 4) || (quantidade > 14)  || ( (quantidade%2) !== 0) ) {
+    quantidade = prompt("Quantidade inválida. Escolha um número par entre 4 e 14 cartas");
+    selecionandoQuantidade()
+}
+
+// embaralhando as cartas
+function comparador() {
+    return Math.random() - 0.5;    //função dada na descrição do projeto
+}
+
+let imagensEmbaralhadas = imagensCartas.slice(0,(quantidade));
+imagensEmbaralhadas.sort(comparador)
+
+// Montando o baralho de cartas no browser
+
+for (let i=0; i < quantidade; i++) {
+    let main = document.querySelector('main')
+    main.innerHTML += `<div class="card" data-identifier="card">
+    <div class="back-face face" data-identifier="back-face">
+        <img src="imagens/front.png" alt="back_card">
+    </div>
+    <div class="front-face face" data-identifier="front-face">
+    <img src=${imagensEmbaralhadas[i]} alt="front_card">
+    </div>
+    </div>
+    `
+
+
+}
+
+
+
+} // fim da função jogar
+
+
+
+jogar()
+
+
+
+//...............................................................................
+
+/*
 let cartas = []
 let carta = "carta"
 for(let i=0; i< quantidade;i++) {
@@ -15,22 +71,14 @@ let conjuntoCartas = document.querySelector(".baralho")
 
 for (let i=0; i < quantidade; i++) {
     conjuntoCartas.innerHTML += ` 
-        <div class="carta-background front-face">
+        <div class="front-face carta-background ">
             <img class="tamanho-papagaio " src="imagens/front.png" alt=""> 
         </div>
-        <div class="carta-background back-face">
+        <div class="back-face carta-background">
             <img class="tamanho-gif" src="imagens/unicornparrot.gif" alt=""> 
         </div>
-    </div>
     `
 }
 
-/*<div class="card">
-      <div class="front-face face">
-        Frente
-      </div>
-      <div class="back-face face">
-        Verso
-      </div>
-    </div> */
-console.log
+} // fim da função jogar
+*/
